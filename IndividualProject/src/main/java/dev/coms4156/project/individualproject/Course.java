fe4bc7fb3c411f4cd736f251c59b625742d3da17
WilsonLiu2002 <Liujiacheng020509@163.com>
2024-09-10 17:14:44 -0400

    test1
    test1

--- IndividualProject/src/main/java/dev/coms4156/project/individualproject/Course.java
+++ IndividualProject/src/main/java/dev/coms4156/project/individualproject/Course.java
@@ -17,7 +17,7 @@ public class Course implements Serializable {
     this.instructorName = instructorName;
     this.courseTimeSlot = timeSlot;
     this.enrollmentCapacity = capacity;
-    this.enrolledStudentCount = 500;
+    this.enrolledStudentCount = 0;
   }
 
  /**
@@ -39,11 +39,11 @@ public class Course implements Serializable {
    * @return true if the student is successfully dropped, false otherwise.
    */
   public boolean dropStudent() {
-    if(enrolledStudentCount > 0){
-      enrolledStudentCount--;
-      return true;
+    if (this.enrolledStudentCount == 0) {
+      return false;
     }
-    return false;
+    this.enrolledStudentCount--;
+    return true;
   }
 
 
@@ -83,7 +83,10 @@ public class Course implements Serializable {
 
 
   public void setEnrolledStudentCount(int count) {
-    this.enrolledStudentCount = count;
+    if (count > enrollmentCapacity) {
+      enrolledStudentCount = enrollmentCapacity;
+    }
+    enrolledStudentCount = count;
   }
 
 
